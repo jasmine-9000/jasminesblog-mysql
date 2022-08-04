704927e2c720de732e6d33faeedc4eaca41b1f0d
Jasmine <wrkerr9@gmail.com>
2022-08-03 17:49:11 -0700

    meow

--- readpostswithcommentsattached.sql
+++ readpostswithcommentsattached.sql
@@ -1,4 +1,4 @@
 # this is how to join two tables with a one to many relationship
 USE MyPosts;
 SELECT * FROM Posts
-	INNER JOIN Comments WHERE Comments.OriginPostID = Posts.PostID AND Posts.PostID = 1;
+	INNER JOIN Comments WHERE Comments.OriginPostID = Posts.PostID AND Posts.PostID = 6;
